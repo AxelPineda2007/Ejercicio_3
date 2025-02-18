fe9ed889aa3190b23face48e04fe93cd53c27869
Axel <axelpineda141@gmail.com>
2025-02-18 10:07:41 -0600

    Add files via upload

---
+++ Ejercicio3CS. 2003634/Ejercicio3CS. 2003634/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3CS._2003634
+{
+    internal class Ordenador
+    {
+        public string Marca { get; set; }
+        public string Procesador { get; set; }
+        public int Memoria { get; set; } 
+        public int DiscoDuro { get; set; } 
+
+        public Ordenador(string marca, string procesador, int memoria, int discoDuro)
+        {
+            Marca = marca;
+            Procesador = procesador;
+            Memoria = memoria;
+            DiscoDuro = discoDuro;
+        }
+
+
+
+
+
+
+        //Controlar el acceso a los atributos de una clase
+        public void MayorCapacidad()
+        {
+            DiscoDuro += 100;
+            Console.WriteLine($"Capacidad del disco duro aumentada a {DiscoDuro} GB.");
+        }
+
+        public void MenorCapacidad()
+        {
+            if (DiscoDuro >= 100)
+            {
+                DiscoDuro -= 100;
+                Console.WriteLine($"Capacidad del disco duro reducida a {DiscoDuro} GB.");
+            }
+            else
+            {
+                Console.WriteLine("No se puede reducir más la capacidad del disco duro.");
+            }
+        }
+    }
+
+}
+
---
+++ Ejercicio3CS. 2003634/Ejercicio3CS. 2003634/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3CS._2003634
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ordenador miOrdenador = new Ordenador("Dell", "Intel i7", 16, 500);
+            Console.WriteLine($"Ordenador: {miOrdenador.Marca}, {miOrdenador.Procesador}, Memoria: {miOrdenador.Memoria}GB, Disco Duro: {miOrdenador.DiscoDuro}GB");
+
+            miOrdenador.MayorCapacidad();
+            miOrdenador.MenorCapacidad();
+            miOrdenador.MenorCapacidad();
+        }
+    }
+}
